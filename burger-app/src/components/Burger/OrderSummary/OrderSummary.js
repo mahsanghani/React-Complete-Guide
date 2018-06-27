31a96b4c42c8cac2fa62e3755abf3a5e5c90aca0
Marty Mammel <mmammel@wiley.com>
2018-06-27 15:55:06 -0400

    Section 8 complete

--- burger-app/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-app/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,26 +1,31 @@
-import React, {Fragment} from 'react';
+import React, { Component, Fragment } from 'react';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
-    return (
-      <li key={igKey}>
-        <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-      </li>)
-  });
-  return (
-    <Fragment>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-      <p>Continue to Checkout?</p>
-      <Button btnType='Danger' clicked={props.purchaseCanceled}>CANCEL</Button>
-      <Button btnType='Success' clicked={props.purchaseContinued}>CONTINUE</Button>
-    </Fragment>
-  );
-};
+class OrderSummary extends Component {
+  // This could be a functional component, doesn't need to be a class
+  
+  render () {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => {
+      return (
+        <li key={igKey}>
+          <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+        </li>)
+    } );
 
-export default orderSummary;
+    return (
+      <Fragment>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+        <p>Continue to Checkout?</p>
+        <Button btnType='Danger' clicked={this.props.purchaseCanceled}>CANCEL</Button>
+        <Button btnType='Success' clicked={this.props.purchaseContinued}>CONTINUE</Button>
+      </Fragment>
+    );
+  }
+} 
+
+export default OrderSummary;
