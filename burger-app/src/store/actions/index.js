d93bb116e562e61c975e35f084bbcc772d42f323
Marty Mammel <mmammel12@gmail.com>
2018-07-25 09:06:39 -0400

    burgerBuilder saga finished

--- burger-app/src/store/actions/index.js
+++ burger-app/src/store/actions/index.js
@@ -1,4 +1,6 @@
 export {
+  setIngredients,
+  setIngredientsFailed,
   addIngredient,
   removeIngredient,
   initIngredients
---
+++ burger-app/src/store/sagas/burgerBuilder.js
@@ -0,0 +1,14 @@
+import { put } from 'redux-saga/effects'
+import axios from '../../axios-orders';
+import * as actions from '../actions/index';
+
+export function* initIngredientsSaga() {
+  try {
+    const response = yield axios.get('https://react-complete.firebaseio.com/ingredients.json');
+    yield put(actions.setIngredients(response.data));
+  }
+  catch (err) {
+    yield console.error(err);
+    yield put(actions.setIngredientsFailed());
+  }
+}
