8fa4d6436c197e4c0f83e3e9d6a906aeff58ca52
Marty Mammel <mmammel12@gmail.com>
2018-07-30 15:08:31 -0400

    advanced react Section 3 Lecture 24

---
+++ advanced-react/testing/src/components/__tests__/App.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../App';
+import CommentBox from '../CommentBox';
+import CommentList from '../CommentList';
+
+it('shows a comment box', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find(CommentBox).length).toEqual(1);
+});
+
+it('shows a comment list', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find(CommentList).length).toEqual(1);
+});
