fee3f7b259e9822a75de87d8a331deeed1931d79
Marty Mammel <mmammel12@gmail.com>
2018-07-31 11:20:06 -0400

    advanded react testing module lecture 37

---
+++ advanced-react/testing/src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import CommentBox from 'components/CommentBox';
+
+let wrapper;
+beforeEach(() => {
+  wrapper = mount(<CommentBox />);
+});
+
+afterEach(() => {
+  wrapper.unmount();
+})
+
+it('has a text area', () => {
+  expect(wrapper.find('textarea').length).toEqual(1);
+});
+
+it('has a button', () => {
+  expect(wrapper.find('button').length).toEqual(1);
+});
+
+describe('the text area', () => {
+  beforeEach(() => {
+    wrapper.find('textarea').simulate('change', {
+      target: {
+        value: 'new comment'
+      }
+    });
+    wrapper.update();
+  })
+
+  it('has a text area that users can type in', () => {
+    expect(wrapper.find('textarea').prop('value')).toEqual('new comment');
+  });
+  
+  it('clears text area when form is submitted', () => {
+    wrapper.find('form').simulate('submit');
+    wrapper.update();
+    expect(wrapper.find('textarea').prop('value')).toEqual('');
+  });
+});
