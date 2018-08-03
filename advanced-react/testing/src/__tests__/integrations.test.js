0a8bfe8d0e78ff921f44b510830e433be136b77f
Marty Mammel <mmammel12@gmail.com>
2018-08-03 12:40:48 -0400

    advanced react Testing module complete

--- advanced-react/testing/src/__tests__/integrations.test.js
+++ advanced-react/testing/src/__tests__/integrations.test.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import moxios from 'moxios';
 import Root from 'Root';
 import App from 'components/App';
 
-it('can fetch a list of comments and display them', () => {
-  // Attempt to render the *entire* app
+beforeEach(() => {
+  moxios.install();
+  moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
+    status: 200,
+    response: [
+      { name: 'Fetch 1' },
+      { name: 'Fetch 2' }
+    ]
+  });
+});
+
+afterEach(() => {
+  moxios.uninstall();
+});
+
+it('can fetch a list of comments and display them', (done) => {
   const wrapper = mount(
     <Root>
       <App />
     </Root>
   );
 
-  // find the 'fetchComments' button and click it
   wrapper.find('.fetch-comments').simulate('click');
 
-  // Expect to find a list of comments
-  expect(wrapper.find('li').length).toEqual(500);
+  moxios.wait(() => {
+    wrapper.update();
+    expect(wrapper.find('li').length).toEqual(2);
+    done();
+    wrapper.unmount();
+  });
 });
